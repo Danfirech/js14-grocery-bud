18594f87148c9f1d96aaae54bca307d7ca8b1c60
Dan Possehl <danielbpossehl@gmail.com>
2021-12-09 15:37:28 -0700

    added innerHTML to app js

--- setup/app.js
+++ setup/app.js
@@ -28,7 +28,22 @@ function addItem(e) {
   const id = new Date().getTime().toString();
   console.log(id);
   if (value && !editFlag) {
-    console.log("add item to list");
+    const element = document.createElement("article");
+    //add class
+    element.classList.add("grocery-item");
+    //add id
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">item</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
   } else if (value && editFlag) {
     console.log("editing");
   } else {
